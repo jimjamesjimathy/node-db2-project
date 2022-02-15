6148fffd05f42b07c0e36c78aa114c573bc2f5ce
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-02-15 17:26:05 -0500

    flushing out the router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    res.json('getting all cars');
+});
+
+router.get('/:id', async (req, res, next) => {
+    res.json(`getting car with ID of ${req.params.id}`);
+});
+
+router.post('/', async (req, res, next) => {
+    res.json('posting a car');
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require("./cars/cars-router");
+const server = express();
 
-const server = express()
+server.use(express.json());
 
+server.use('/api/cars', carsRouter);
+
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'nope'});
+});
+
+server.use((err, req, res, next) => {// eslint-disable-line 
+    res.status(err.status || 500).json({ 
+        message: err.message
+    });
+});
 // DO YOUR MAGIC
 
-module.exports = server
+module.exports = server;
