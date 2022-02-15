a1f1318bd7c392a181a2dcf92b0057d9c6793e2f
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-02-15 17:33:31 -0500

    getting all cars back

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const Car = require('./cars-model');
+
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars');
+    try {
+        const cars = await Car.getAll();
+        res.json(cars);
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.get('/:id', async (req, res, next) => {
